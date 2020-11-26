b40f5192c04d52d8ee9ad033bd6297b92b5b6c3a
rubens <rubens@rubens-HP-Pavilion-dv4-Notebook-PC>
2020-11-25 21:51:44 -0300

    Nome do projeto mudado, conexao com bd iniciada com as classes de Funcionario e FuncionarioDao

---
+++ src/Funcionario.java
@@ -0,0 +1,96 @@
+public class Funcionario {
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    private String rg;
+    private String rua;
+    private int numero;
+    private String bairro;
+    private String telefone;
+    private int codBanhotosa;
+
+    public Funcionario(String nome, String sobrenome, String cpf, String rg, String rua, int numero, String bairro, String telefone, int codBanhotosa) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.rua = rua;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.telefone = telefone;
+        this.codBanhotosa = codBanhotosa;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getCodBanhotosa() {
+        return codBanhotosa;
+    }
+
+    public void setCodBanhotosa(int codBanhotosa) {
+        this.codBanhotosa = codBanhotosa;
+    }    
+    
+}
